5fd776b6253972b62834a6c23d7afb899d82eb87
Mirzamehdi <mirzemehdi@gmail.com>
2024-09-29 02:21:31 +0200

    Move SHOW_REMOTE_PAYWALL to Constants

--- composeApp/src/commonMain/kotlin/com/measify/kappmaker/presentation/screens/paywall/PaywallScreenRoute.kt
+++ composeApp/src/commonMain/kotlin/com/measify/kappmaker/presentation/screens/paywall/PaywallScreenRoute.kt
@@ -4,15 +4,15 @@ import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.measify.kappmaker.presentation.screens.paywall.remotepaywall.RemotePaywallScreen
+import com.measify.kappmaker.util.Constants
 import com.measify.kappmaker.util.ScreenRoute
 import com.measify.kappmaker.util.uiStateHolder
 
 class PaywallScreenRoute : ScreenRoute {
     @Composable
     override fun Content() {
-        val showRemotePaywall = true // Change to false for presenting custom paywall screen
         val navigator = LocalNavigator.currentOrThrow
-        if (showRemotePaywall) {
+        if (Constants.SHOW_REMOTE_PAYWALL) {
             RemotePaywallScreen(onDismiss = { navigator.pop() })
         } else {
             val uiStateHolder = uiStateHolder<PaywallUiStateHolder>()
--- composeApp/src/commonMain/kotlin/com/measify/kappmaker/util/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/measify/kappmaker/util/Constants.kt
@@ -3,5 +3,7 @@ package com.measify.kappmaker.util
 object Constants {
     const val URL_PRIVACY_POLICY = ""
     const val URL_TERMS_CONDITIONS = ""
+
     const val PAYWALL_PREMIUM_ENTITLEMENT = "Premium"
+    const val SHOW_REMOTE_PAYWALL = true // Change to false for presenting custom paywall screen
 }
