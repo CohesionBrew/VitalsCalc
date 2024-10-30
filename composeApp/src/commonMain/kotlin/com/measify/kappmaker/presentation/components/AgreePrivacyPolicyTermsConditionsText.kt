39e0d9462c696e42f461eff94a4939adcd52f814
Mirzamehdi Karimov <32781662+mirzemehdi@users.noreply.github.com>
2024-10-30 22:03:34 +0400

    Bump dependencies (#1)
    * Bumping dependencies

--- composeApp/src/commonMain/kotlin/com/measify/kappmaker/presentation/components/AgreePrivacyPolicyTermsConditionsText.kt
+++ composeApp/src/commonMain/kotlin/com/measify/kappmaker/presentation/components/AgreePrivacyPolicyTermsConditionsText.kt
@@ -3,14 +3,17 @@ package com.measify.kappmaker.presentation.components
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
 import com.measify.kappmaker.util.Constants
 import com.measify.kappmaker.generated.resources.Res
@@ -20,45 +23,11 @@ import com.measify.kappmaker.generated.resources.terms_conditions
 import com.measify.kappmaker.generated.resources.txt_agree_privacy_policy_and_terms
 import org.jetbrains.compose.resources.stringResource
 
-//TODO: Uncomment below when composer version 1.7.x is stable
-
-//@Composable
-//fun AgreePrivacyPolicyTermsConditionsText(modifier: Modifier) {
-//    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-//        val privacyPolicy = stringResource(Res.string.privacy_policy)
-//        val termsConditions = stringResource(Res.string.terms_conditions)
-//        val customStyle = SpanStyle(
-//            fontWeight = FontWeight.SemiBold,
-//            color = MaterialTheme.colorScheme.secondary,
-//            textDecoration = TextDecoration.Underline
-//        )
-//        val annotatedString = buildAnnotatedString {
-//            append(stringResource(Res.string.txt_agree_privacy_policy_and_terms))
-//
-//            withStyle(style = customStyle) {
-//                withLink(LinkAnnotation.Url(url = Constants.URL_PRIVACY_POLICY)) {
-//                    append(privacyPolicy)
-//                }
-//            }
-//            append(" ${stringResource(Res.string.and)} ")
-//            withStyle(customStyle) {
-//                withLink(LinkAnnotation.Url(url = Constants.URL_TERMS_CONDITIONS)) {
-//                    append(termsConditions)
-//                }
-//            }
-//
-//        }
-//        Text(text = annotatedString, style = MaterialTheme.typography.bodyMedium)
-//    }
-//}
-
-//TODO:Replace with above code when composer version 1.7.x is stable
 @Composable
 fun AgreePrivacyPolicyTermsConditionsText(modifier: Modifier) {
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         val privacyPolicy = stringResource(Res.string.privacy_policy)
         val termsConditions = stringResource(Res.string.terms_conditions)
-        val localUriHandler = LocalUriHandler.current
         val customStyle = SpanStyle(
             fontWeight = FontWeight.SemiBold,
             color = MaterialTheme.colorScheme.secondary,
@@ -66,37 +35,20 @@ fun AgreePrivacyPolicyTermsConditionsText(modifier: Modifier) {
         )
         val annotatedString = buildAnnotatedString {
             append(stringResource(Res.string.txt_agree_privacy_policy_and_terms))
+
             withStyle(style = customStyle) {
-                pushStringAnnotation(
-                    tag = privacyPolicy,
-                    annotation = privacyPolicy
-                )
-                append(privacyPolicy)
+                withLink(LinkAnnotation.Url(url = Constants.URL_PRIVACY_POLICY)) {
+                    append(privacyPolicy)
+                }
             }
             append(" ${stringResource(Res.string.and)} ")
-            withStyle(style = customStyle) {
-                pushStringAnnotation(
-                    tag = termsConditions,
-                    annotation = termsConditions
-                )
-                append(termsConditions)
+            withStyle(customStyle) {
+                withLink(LinkAnnotation.Url(url = Constants.URL_TERMS_CONDITIONS)) {
+                    append(termsConditions)
+                }
             }
 
         }
-
-        ClickableText(
-            text = annotatedString, style = MaterialTheme.typography.bodyMedium.copy(
-                color = MaterialTheme.colorScheme.onBackground,
-                fontWeight = FontWeight.Medium
-            )
-        ) { offset ->
-            annotatedString.getStringAnnotations(offset, offset)
-                .firstOrNull()?.let { span ->
-                    when (span.item) {
-                        privacyPolicy -> localUriHandler.openUri(Constants.URL_PRIVACY_POLICY)
-                        termsConditions -> localUriHandler.openUri(Constants.URL_TERMS_CONDITIONS)
-                    }
-                }
-        }
+        Text(text = annotatedString, style = MaterialTheme.typography.bodyMedium)
     }
 }
