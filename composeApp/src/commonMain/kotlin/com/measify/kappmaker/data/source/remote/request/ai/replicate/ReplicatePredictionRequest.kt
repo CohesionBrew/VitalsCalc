1dad5d09aeeb85bf18028d23d8c256228d908744
Mirzamehdi <mirzemehdi@gmail.com>
2025-01-06 01:33:47 +0100

    Setting replicate predictiction request version nullable

--- composeApp/src/commonMain/kotlin/com/measify/kappmaker/data/source/remote/request/ai/replicate/ReplicatePredictionRequest.kt
+++ composeApp/src/commonMain/kotlin/com/measify/kappmaker/data/source/remote/request/ai/replicate/ReplicatePredictionRequest.kt
@@ -6,10 +6,11 @@ import kotlinx.serialization.Serializable
 /**
  * Represents a request to create a prediction on a Replicate model.
  * Some models require version of the model to be specified, while others don't.
+ * If you used create model prediction, then you need to pass null for version.
  */
 
 @Serializable
 data class ReplicatePredictionRequest<Input>(
-    @SerialName("version") val version: String = "",
+    @SerialName("version") val version: String? = null,
     @SerialName("input") val input: Input,
 )
